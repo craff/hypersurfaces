da564328a2b8d0a8e1f9333fbead97a5961710eb
craff <christophe@raffalli.eu>
2021-05-22 17:52:16 -0900

    comments and improvment in Previous.ml(i)

--- src/Previous.ml
+++ src/Previous.ml
@@ -9,7 +9,30 @@ let add : type a. a -> a previous -> unit = fun x r ->
   r.data.(r.current) <- Some x;
   r.current <- (r.current + 1) mod Array.length r.data
 
-let get : type a. a previous -> a = fun r ->
-  match r.data.(r.current) with
+let get : type a. ?pos:int -> a previous -> a = fun ?(pos= (-1)) r ->
+  let i = r.current - 1 - pos in
+  let i = i mod (Array.length r.data) in
+  let i = if i < 0 then i + Array.length r.data else i in
+  match r.data.(i) with
   | Some x -> x
   | None -> raise Not_found
+
+let max_size : type a. a previous -> int = fun q -> Array.length q.data
+
+let size : type a. a previous -> int = fun q ->
+  let len = Array.length q.data in
+  if q.data.(0) = None then 0
+  else if q.data.(len-1) <> None then len
+  else
+    (* loop invariant: 0 <= a < b < len
+                       q.data.(a) <> None,
+                       q.data.(b) = None *)
+    let rec fn a b =
+      if a = b - 1 then b
+      else
+        let c = (a + b) / 2 in
+        if q.data.(c) = None then
+          fn a c
+        else fn c b
+    in
+    fn 0 (len - 1)
--- src/Previous.mli
+++ src/Previous.mli
@@ -1,7 +1,27 @@
 type 'a previous
 
+(** [create n] create a queue of at most [n] elements. *)
 val create : int -> 'a previous
 
+(** [add x q] add [x] to the queue [q]. If [q] would
+    have more that [n] elements, then the oldest added
+    element is automatically removed *)
 val add : 'a -> 'a previous -> unit
 
-val get : 'a previous -> 'a
+(** [get ~pos:n q] returns the nth element of the queue.
+    [get ~pos:0 q] means the latest added element.
+    [get ~pos:1 q] means the element before [].
+    ...
+    [get ~pos:(-1) q] (the default) means the oldest element in the queue.
+    [n] is taken module the maximum size of the queue.
+
+    [get ~pos q] may raise Not_found if the requested element do not exists in
+    then queue. *)
+val get : ?pos:int -> 'a previous -> 'a
+
+(** [max_size q] returns maximum number of elements in the queue *)
+val max_size : 'a previous -> int
+
+(** [size q] returns the current number of elements in the queue (log time
+    in [max_size q], as the current size if not stored *)
+val size : 'a previous -> int
